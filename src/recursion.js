6cde2abb37c020e82e8ce31234ff3333b2184533
christinameador <christinameador1@gmail.com>
2020-08-28 07:20:50 -0500

    completed problem 3 (arraySum)

--- src/recursion.js
+++ src/recursion.js
@@ -26,6 +26,15 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+    let total = 0
+    for (var i=0; i<array.length; i++){
+        if (Array.isArray(array[i])){
+            total+=arraySum(array[i])
+        } else {
+            total+=array[i]
+        }
+    }
+    return total
 };
 
 // 4. Check if a number is even.
