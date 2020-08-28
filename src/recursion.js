046ff2f42cf16550be83c7875159e990cad8f74f
christinameador <christinameador1@gmail.com>
2020-08-28 07:03:14 -0500

    completed problem 4 (isEven)

--- src/recursion.js
+++ src/recursion.js
@@ -30,6 +30,14 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+    if(n===1){
+        return false;
+      } else if(n===0){
+        return true;
+      } else if(n<0){
+        n = -n;
+      }
+      return isEven(n-2);
 };
 
 // 5. Sum all integers below a given integer.
